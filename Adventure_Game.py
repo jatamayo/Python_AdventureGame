0abe7c1150756fcb82847b2cab4b41b651721f4a
jatamayo <jalejandrotamayo@hotmail.com>
2019-01-12 10:44:15 -0600

    start to add game descriptions

--- Adventure_Game.py
+++ Adventure_Game.py
@@ -1,7 +1,7 @@
+# -*- coding: utf-8 -*-
 import time
 import random
 import os
-from os import system, name
 
 #------------------------------------------------
 #----------  GLOBAL VARIABLES -------------------
@@ -12,17 +12,20 @@ list_answers = 1
 list_correct_answers = 2
 list_incorrect_answers = 3
 list_description_level = 4
+list_tittle = 5
 
 #------------------------------------------------
 #----------  INFORMATION FOR 3 LEVELS -----------
 #------------------------------------------------
 
+
+
 list_dance_battle = [
-    ['First question option 1','First question option 2','First question option 3'],
-    ['First answer option 1','First answer option 2','First answer option 3'],
-    ['First correct answer option 1','First correct answer option 2','First correct answer option 3'],
-    ['First incorrect answer option 1','First incorrect answer option 2','First incorrect answer option 3'],
-    ['First description level 1','First description level  2','First description level  3']
+    ['Wow.. The opponent is dancing the -TAKE THE L- dance \nWhat are you going to do?\n','Wow.. The opponent is dancing the -FRESH- dance \nWhat are you going to do?\n','Wow.. The opponent is dancing the -BEST MATES- dance \nWhat are you going to do?\n'],
+    ['1-Run away from the battle?\n2-Accept the challenge and doing the dance - THE WORM -?','1-Run away from the battle?\n2-Accept the challenge and doing the dance - LLAMA BELL -?','1-Run away from the battle?\n2-Accept the challenge and doing the dance - WIGGLE -?'],
+    ['Wow.. that is a really dance battle. excellent we have impressed to your opponent. Now we are ready to go to the battle.'],
+    ['oh, we havent impressed your opponent, before to go to the battle we need to win our battle dance. lets try again.'],
+    ['Hello, welcome to the world of -FORTNITE-. are you ready to start the battle.','To start we are going to go to the first stage, before start the battle we need to wait for a seconds.','Wait a momment, what is that? a player wants to have a dance battle. Ok lets start.']
 ]
 
 list_treasure_chest = [
@@ -30,7 +33,7 @@ list_treasure_chest = [
     ['Second answer option 1','Second answer option 2','Second answer option 3'],
     ['Second correct answer option 1','Second correct answer option 2','Second correct answer option 3'],
     ['Second incorrect answer option 1','Second incorrect answer option 2','Second incorrect answer option 3'],
-    ['Second description level 1','Second description level 2','Second description level 3']
+    ['Great. now we are on -battle royale-, the first thing we need to do is go to find some treasure.','Great we have found a treasure. lets to open it','oh wow, we have find some weapons, its time to choice what weapon we need to go to the battle.']
 ]
 
 list_win_or_die = [
@@ -45,28 +48,16 @@ list_win_or_die = [
 #----------  GENERAL FUNCTIONS ------------------
 #------------------------------------------------
 
-def clear(): 
-  
-    # for windows 
-    if name == 'nt': 
-        _ = system('cls') 
-  
-    # for mac and linux(here, os.name is 'posix') 
-    else: 
-        _ = system('clear')
-
-
 def Description_Level(list_answers_questions):
     '''
         Function behavior:  
         Inputs:
         Outputs:
     '''
-    print ("Description level")
     for index in range(len(list_answers_questions[list_description_level])):
         time.sleep(3)
         print(list_answers_questions[list_description_level][index])
-    print ("\n\n")
+    print ("\n")
     time.sleep(3)
     print(random.choice(list_answers_questions[list_questions]))
     time.sleep(2)
@@ -80,7 +71,7 @@ def Input_Validation():
     '''
     condition = False
     while True:
-        user_option = input("Now it's time to make a decision, 1 or 2:")
+        user_option = input("\nWhat are you going to do? option 1 or option 2?")
         if user_option == "1" or user_option == "2":
             return user_option  
             condition = True
@@ -112,24 +103,44 @@ def Play_Again():
 #----------  MAIN GAME FUNCTIONS ----------------
 #------------------------------------------------
 
+battle_dance_tittle = '''   
+██████╗  █████╗ ████████╗████████╗██╗     ███████╗    ██████╗  █████╗ ███╗   ██╗ ██████╗███████╗
+██╔══██╗██╔══██╗╚══██╔══╝╚══██╔══╝██║     ██╔════╝    ██╔══██╗██╔══██╗████╗  ██║██╔════╝██╔════╝
+██████╔╝███████║   ██║      ██║   ██║     █████╗      ██║  ██║███████║██╔██╗ ██║██║     █████╗  
+██╔══██╗██╔══██║   ██║      ██║   ██║     ██╔══╝      ██║  ██║██╔══██║██║╚██╗██║██║     ██╔══╝  
+██████╔╝██║  ██║   ██║      ██║   ███████╗███████╗    ██████╔╝██║  ██║██║ ╚████║╚██████╗███████╗
+╚═════╝ ╚═╝  ╚═╝   ╚═╝      ╚═╝   ╚══════╝╚══════╝    ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝╚══════╝'''
+
 def Dance_Battle(list_answers_questions):
     '''
         Function behavior:  
         Inputs:
         Outputs:
     '''
+    os.system('cls')  # on windows - if you are using windows comment the -clear - functions and use this one
+    # os.system('clear')  # on linux / os x - if you are using linux comment the -cls - functions and use this one
+    print (battle_dance_tittle)
     time.sleep(3)
     Description_Level(list_answers_questions)
     user_answer = Input_Validation()
     print (user_answer)
     if user_answer == "1":
         print(random.choice(list_answers_questions[list_correct_answers]))
+        time.sleep(8)
         Treasure_Chest(list_treasure_chest)
-        clear()
     else:
         print(random.choice(list_answers_questions[list_incorrect_answers]))
+        time.sleep(8)
         Dance_Battle(list_dance_battle)
-        clear()
+        
+
+treasure_chest_tittle = '''
+████████╗██████╗ ███████╗ █████╗ ███████╗██╗   ██╗██████╗ ███████╗     ██████╗██╗  ██╗███████╗███████╗████████╗
+╚══██╔══╝██╔══██╗██╔════╝██╔══██╗██╔════╝██║   ██║██╔══██╗██╔════╝    ██╔════╝██║  ██║██╔════╝██╔════╝╚══██╔══╝
+   ██║   ██████╔╝█████╗  ███████║███████╗██║   ██║██████╔╝█████╗      ██║     ███████║█████╗  ███████╗   ██║   
+   ██║   ██╔══██╗██╔══╝  ██╔══██║╚════██║██║   ██║██╔══██╗██╔══╝      ██║     ██╔══██║██╔══╝  ╚════██║   ██║   
+   ██║   ██║  ██║███████╗██║  ██║███████║╚██████╔╝██║  ██║███████╗    ╚██████╗██║  ██║███████╗███████║   ██║   
+   ╚═╝   ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚══════╝     ╚═════╝╚═╝  ╚═╝╚══════╝╚══════╝   ╚═╝'''
 
 def Treasure_Chest(list_answers_questions):
     '''
@@ -137,6 +148,8 @@ def Treasure_Chest(list_answers_questions):
         Inputs:
         Outputs:
     '''
+    os.system('cls')  # on windows - if you are using windows comment the -clear - functions and use this one
+    # os.system('clear')  # on linux / os x - if you are using linux comment the -cls - functions and use this one
     time.sleep(3)
     Description_Level(list_answers_questions)
     user_answer = Input_Validation()
@@ -148,12 +161,22 @@ def Treasure_Chest(list_answers_questions):
         print(random.choice(list_answers_questions[list_incorrect_answers]))
         Treasure_Chest(list_treasure_chest)
 
+win_or_die_tittle = '''
+██╗    ██╗██╗███╗   ██╗     ██████╗ ██████╗     ██████╗ ██╗███████╗
+██║    ██║██║████╗  ██║    ██╔═══██╗██╔══██╗    ██╔══██╗██║██╔════╝
+██║ █╗ ██║██║██╔██╗ ██║    ██║   ██║██████╔╝    ██║  ██║██║█████╗  
+██║███╗██║██║██║╚██╗██║    ██║   ██║██╔══██╗    ██║  ██║██║██╔══╝  
+╚███╔███╔╝██║██║ ╚████║    ╚██████╔╝██║  ██║    ██████╔╝██║███████╗
+ ╚══╝╚══╝ ╚═╝╚═╝  ╚═══╝     ╚═════╝ ╚═╝  ╚═╝    ╚═════╝ ╚═╝╚══════╝'''
+ 
 def Win_or_Die(list_answers_questions):
     '''
         Function behavior:  
         Inputs:
         Outputs:
     '''
+    os.system('cls')  # on windows - if you are using windows comment the -clear - functions and use this one
+    # os.system('clear')  # on linux / os x - if you are using linux comment the -cls - functions and use this one
     time.sleep(3)
     Description_Level(list_answers_questions)
     user_answer = Input_Validation()
